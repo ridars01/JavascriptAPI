428008407460a4d8f0d97dde2953bafdc91073f2
Rida Fatimah <ridafatimah@MacBook-Pro-3.local>
2020-07-26 13:59:59 -0400

    connected todo input to api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser') ; 
 
 const api = express();
 api.use(express.static(__dirname + '/public'));
+api.use(bodyParser.json());
 
 
 api.listen(3000, () => {
   console.log('API up and running!');
 });
+
+api.post('/add', (req, res) => {
+    console.log(req.body);
+    res.send('It works!');
+   });
 /*
 api.get('/', (req, res) => {
     console.log(req);
