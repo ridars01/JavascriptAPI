4f989628fa24403003a6301fb66d3d344f4047a1
Rida Fatimah <ridafatimah@MacBook-Pro-3.local>
2020-07-27 16:17:59 -0400

    Adding final

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser') ; 
+const mysql = require('mysql2');
+require('dotenv').config() ; 
 
 const api = express();
 api.use(express.static(__dirname + '/public'));
@@ -12,8 +14,29 @@ api.listen(3000, () => {
 
 api.post('/add', (req, res) => {
     console.log(req.body);
-    res.send('It works!');
+    
+    connection.query('INSERT INTO tasks (description) VALUES (?)', [req.body.item], (error, results) => {
+     if (error) return res.json({ error: error });
+   connection.query('SELECT LAST_INSERT_ID() FROM tasks', (error, results) => {
+      if (error) return res.json({ error: error });
+   console.log(results);
+     });
+    });
    });
+
+   const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password1',
+    database: 'todo' //dont have this yet
+   });
+
+   try {
+    connection.connect();
+   } catch (e) {
+    console.log('Oops. Connection to MySQL failed.');
+    console.log(e);
+   }
 /*
 api.get('/', (req, res) => {
     console.log(req);
